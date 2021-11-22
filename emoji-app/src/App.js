1bcec9e7a63cdc8dcf276041aaee8d5210444b99
dipen2000 <dipenchavda2000@gmail.com>
2021-11-22 10:54:02 +0530

    initial commit

---
+++ emoji-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Emoji Dictionary</h1>
+      <input type="text"/>
+    </div>
+  );
+}
+
+export default App;
