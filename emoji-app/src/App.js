7661745284f32518117386cd7112400cfabee300
dipen2000 <dipenchavda2000@gmail.com>
2021-11-22 15:33:43 +0530

    Update app.js

--- emoji-app/src/App.js
+++ emoji-app/src/App.js
@@ -37,18 +37,33 @@ function App() {
   const [meaning , setMeaning] = useState("");
   function inputChangeHandler(event){
     let emojiMeaning = emojiDatabase[event.target.value];
+    console.log(emojiMeaning);
+    if(event.target.value === ""){
+      emojiMeaning = "";
+      setMeaning(emojiMeaning);
+    }
+    if(emojiMeaning === undefined){
+      emojiMeaning = "We don't have that emoji in our database";
+      setMeaning(emojiMeaning);
+    }
     setMeaning(emojiMeaning);
+  }
+
+  function clickEmojiHandler(event){
+      let clickedEmoji = event.target.innerText;
+      let emojiMeaning = emojiDatabase[clickedEmoji];
+      setMeaning(emojiMeaning);
   }
   return (
     <div className="App">
       <h1 className="heading">Emoji Dictionary</h1>
-      <input type="text" className="input-ele" onChange={inputChangeHandler} placeholder="type in the emoji here"/>
+      <input type="text" className="input-ele" onChange={inputChangeHandler}  placeholder="type in the emoji here"/>
       <h2 className="font-change">Meaning : {meaning}</h2>
       <h2 className="font-change">Emojis we know!</h2>
       <div className="emojiContainer">
       {
           emojiArr.map((item)=>{
-            return <span className="emoji">{item}</span>
+            return <span className="emoji" onClick={clickEmojiHandler}>{item}</span>
           })
       }
       </div>
