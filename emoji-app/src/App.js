c68127d5ff3f7fad4447a01b1a402d92a54c2230
dipen2000 <dipenchavda2000@gmail.com>
2021-11-22 11:05:45 +0530

    Add emoji data

--- emoji-app/src/App.js
+++ emoji-app/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+let emojiDatabase = {
+  "😎" : "cool",
+  "🤩" : "starstruck",
+  "😉" : "wink",
+  "🤔" : "Thinking",
+  "😊" : "Happy face",
+  "😢" : "Teary eyes",
+  "😂" : "LOL",
+  "😍" : "Heart eyes",
+  "😮" : "Shocked",
+  "😴" : "Sleepy face"
+};
+
 function App() {
+  function inputChangeHandler(event){
+    console.log(event.target.value);
+  }
   return (
     <div className="App">
       <h1>Emoji Dictionary</h1>
-      <input type="text"/>
+      <input type="text" onChange={inputChangeHandler}/>
     </div>
   );
 }
